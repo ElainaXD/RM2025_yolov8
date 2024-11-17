b07f8f796c5b56d75728bc58b78f94ac4cfd3401
wyh <wyhao@163.com>
2024-11-17 12:07:52 +0800

    成功跑完训练流程，还没测试pt文件

---
+++ BuffTraining/train.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    # net_yaml_path = r'D:\Github\RM2025_yolov8\BuffTraining\BuffYaml\net_sturcture.yaml'
+    # data_yaml_path = r'D:\Github\RM2025_yolov8\BuffTraining\BuffYaml\win_kpt.yaml'
+    # cfg_yaml_path = r'D:\Github\RM2025_yolov8\BuffTraining\BuffYaml\hyp.win.yaml'
+
+    net_yaml_path = "./BuffYaml/net_sturcture.yaml"
+    data_yaml_path = "./BuffYaml/win_kpt.yaml"
+    cfg_yaml_path = "./BuffYaml/hyp.win.yaml"
+    # 创建YOLO模型实例
+    model = YOLO(net_yaml_path)
+
+    # 确保模型被移动到CUDA设备上
+    model.to('cuda')
+
+    # 开始训练模型，确保参数设置正确
+    model.train(
+        epochs=100,
+        data=data_yaml_path,
+        device='cuda',  # 指定训练设备为CUDA
+        batch=16,
+        cfg=cfg_yaml_path
+    )
